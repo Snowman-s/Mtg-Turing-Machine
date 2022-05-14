292cb914cd9485e8c524687eace169abca8fb8ee
Snowman-s <snowman.snowsnow@gmail.com>
2022-05-14 11:14:54 +0900

    Setup Main

---
+++ src/main/java/snowesamosc/Main.java
@@ -0,0 +1,19 @@
+package snowesamosc;
+
+import processing.core.PApplet;
+
+public class Main extends PApplet {
+    public static void main(String[] args) {
+        PApplet.main("snowesamosc.Main");
+    }
+
+    @Override
+    public void settings() {
+        size(500,500);
+    }
+
+    @Override
+    public void draw() {
+        background(0);
+    }
+}
